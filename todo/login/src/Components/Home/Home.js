f1361888de2f3ac52afb54580425d732e8f78910
Shanmuk <shanmukha.anipaddi@adsiduous.com>
2022-01-28 19:30:45 +0530

    second try

---
+++ todo/login/src/Components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from './Home.module.css';
+
+
+const Home = (props) => {
+    return(
+        <div className={classes.home}>This Is Dashboard</div>
+    );
+}
+
+export default Home;
